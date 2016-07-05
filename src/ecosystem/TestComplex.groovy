b632df032229263ede88f08862ddb9c7c43ee4bf
sky <diufanshu@gmail.com>
2016-07-05 18:45:33 +0800

    开始着手进一步设计

---
+++ src/ecosystem/TestComplex.groovy
@@ -0,0 +1,14 @@
+package ecosystem
+
+class TestComplex {
+    class Demander extends PureDemander{
+        
+    }
+    
+    class Provider extends PureProvider{
+        
+    }
+    
+    Demander demander = new Demander()
+    Provider provider = new Provider()
+}
